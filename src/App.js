dea9274ad1c3050a25eab2a424d6957e395af905
Daniel Nunez <daniel.nunez@nave.rs>
2020-09-21 15:43:52 -0300

    add formdata req

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const isFileType = value =>
+  typeof value === 'object' &&
+  Object.getPrototypeOf(value)
+    .toString()
+    .includes('File')
+
+const formatToFormdata = (values) => {
+  const formData = new FormData()
+  Object.entries(values).forEach(([name, value]) => {
+    if(!isFileType(value)) {
+      return formData.append(name, value)
+    }
+    if(value.length <= 1) {
+      return formData.append(name, value[0] || '')
+    }
+    return formData.append(`${name}`, value || '')
+  })
+  return formData
 }
 
-export default App;
+const App = () => {
+
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = useCallback(async (values) => {
+    try {
+      const serializedData = formatToFormdata(values)
+      await axios.post('/lorem/ipsum', serializedData, { 'Content-Type': 'multipart/form-data' })
+    } catch (e) {
+      console.log(e)
+    }
+  }, [])
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p>texto</p>
+      <input type='text' placeholder='text' name='txt' ref={register}/>
+      <p>file</p>
+      <input type='file' name='normal' ref={register}/>
+      <p>multiple file</p>
+      <input type='file' name='multiplo' multiple ref={register}/>
+      <button>submit</button>
+    </form>
+  )
+}
+
+export default App 
